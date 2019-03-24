d1ea1d445b0cce5f4e2769c0ea0571d98fa3545d
nirko <nir.kovalio@gmail.com>
2019-03-24 14:46:13 +0200

    update

--- compress.sh
+++ compress.sh
@@ -7,7 +7,7 @@ shopt -s nocaseglob
 for file in *.{mp4,wmv,avi,mpeg,mpg,mkv,mov,m4v}; do
 		echo ${file};
      	ffmpeg -i "$file" -f ffmetadata "$file"meta.txt
-     	ffmpeg -i "$file" -f ffmetadata -i "$file"meta.txt -c:v libx265 -preset medium -x265-params crf=28 -c:a aac -strict experimental -b:a 128k ${file%%.*}.h265.mp4
+     	ffmpeg -i "$file" -f ffmetadata -i "$file"meta.txt -map_metadata 0:g -c:v libx265 -preset medium -x265-params crf=28 -c:a aac -strict experimental -b:a 128k "${file%%.*}.h265.mp4"
      	rm "$file"meta.txt
      i=$((i+1))
 done
